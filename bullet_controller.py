d755ecfa43d79914d24a931872570b56b7f0c800
Sofia Rytik <dv11@3soft.ru>
2021-09-11 13:40:49 +0300

    one bullet can kill only one monster

--- bullet_controller.py
+++ bullet_controller.py
@@ -10,7 +10,7 @@ class BulletController(Controller):
     def __init__(self, screen: mainsc.MainScreen):
         self.bullets = []
         self.screen = screen
-        #self.spaceship = spaceship
+        # self.spaceship = spaceship
 
     def create_bullet(self, x, y):
         bullet = Bullet(x, y)
@@ -23,17 +23,19 @@ class BulletController(Controller):
     def process(self, delta_time):
         for bullet in self.bullets:
             bullet.move(bullet.x, bullet.y + (-1 * bullet.speed * delta_time))
+            if bullet.y < 20:
+                self.screen.banish(bullet)
             for game_obj in self.screen.game_objects:
                 # if game_obj.get_x() == bullet.x and game_obj.get_y() == bullet.y and game_obj is not bullet: # см класс game_object
-                #if game_obj is not bullet and game_obj is not Spaceship and bullet.check_crossing(game_obj):
+                # if game_obj is not bullet and game_obj is not Spaceship and bullet.check_crossing(game_obj):
                 if game_obj is not bullet and bullet.check_crossing(game_obj) and not isinstance(game_obj, Spaceship):
                     self.screen.banish(bullet)  # добавить в banish удаление из контроллера
                     # self.bullets.remove(bullet)
                     self.screen.banish(game_obj)
-            if bullet.y < 20:
-                self.screen.banish(bullet)
-                self.bullets.remove(bullet)
+            # if bullet.y < 20:
+            #     self.screen.banish(bullet)
+            #     self.bullets.remove(bullet)
 
-    # def banish(self, game_object: GameObject):
-    #     if game_object in self.bullets:
-    #         self.bullets.remove(game_object)
+    def banish(self, game_object: GameObject):
+        if game_object in self.bullets:
+            self.bullets.remove(game_object)
